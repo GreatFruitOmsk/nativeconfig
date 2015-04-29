f7ac1478dc3da4c2571ccff2be85333d900b795c
Ilya Kulakov <kulakov.ilya@gmail.com>
2015-04-29 16:39:07 +0600

    Add tests for RegistryConfig. #andy-561

--- nativeconfig/config/registry_config.py
+++ nativeconfig/config/registry_config.py
@@ -11,7 +11,7 @@ ERROR_NO_MORE_ITEMS = 259
 ERROR_NO_MORE_FILES = 18
 
 
-def _traverse_registry_key(key, sub_key):
+def traverse_registry_key(key, sub_key):
     """
     Traverse registry key and yield one by one.
 
@@ -21,16 +21,24 @@ def _traverse_registry_key(key, sub_key):
 
     try:
         i = 0
+
         while True:
             next_key = winreg.EnumKey(current_key, i)
-            for k in _traverse_registry_key(key, r'{}\{}'.format(sub_key, next_key)):
+
+            for k in traverse_registry_key(key, r'{}\{}'.format(sub_key, next_key)):
                 yield k
+
             i += 1
-    except WindowsError:
+    except OSError:
         yield sub_key
 
 
 class RegistryConfig(BaseConfig):
+    """
+    Stores config in Windows Registry.
+
+    @cvar REGISTRY_KEY: Key that followed by CONFIG_PATH points to location of config. I.e. REGISTRY_KEY\CONFIG_PATH is the full path.
+    """
     LOG = LOG.getChild('RegistryConfig')
     REGISTRY_KEY = winreg.HKEY_CURRENT_USER
 
@@ -40,88 +48,88 @@ class RegistryConfig(BaseConfig):
         k = winreg.CreateKey(self.REGISTRY_KEY, self.CONFIG_PATH)
         winreg.CloseKey(k)
 
+#{ BaseConfig
+
     def get_value(self, name):
         try:
             with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH) as app_key:
                 try:
-                    return str(winreg.QueryValueEx(app_key, name)[0])
-                except:
-                    self.LOG.info("Unable to get '%s' from the registry:", name, exc_info=True)
+                    value, value_type = winreg.QueryValueEx(app_key, name)
+
+                    if not value_type == winreg.REG_SZ:
+                        raise ValueError("Value must be of REG_SZ type!")
+
+                    return value
+                except OSError:
+                    pass
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.exception("Unable to get \"%s\" from the registry:", name)
 
         return None
 
     def set_value(self, name, raw_value):
         try:
             with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH, 0, winreg.KEY_WRITE) as app_key:
-                try:
-                    winreg.SetValueEx(app_key, name, 0, winreg.REG_SZ, str(raw_value))
-                except:
-                    self.LOG.exception("Unable to set '%s' in the registry:", name)
+                winreg.SetValueEx(app_key, name, 0, winreg.REG_SZ, raw_value)
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.exception("Unable to set \"%s\" in the registry:", name)
 
     def del_value(self, name):
         try:
             try:
-                for k in _traverse_registry_key(self.REGISTRY_KEY, r'{}\{}'.format(self.CONFIG_PATH, name)):
+                for k in traverse_registry_key(self.REGISTRY_KEY, r'{}\{}'.format(self.CONFIG_PATH, name)):
                     winreg.DeleteKey(self.REGISTRY_KEY, k)
-            except WindowsError:
-                try:
-                    with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH, 0, winreg.KEY_ALL_ACCESS) as app_key:
-                        winreg.DeleteValue(app_key, name)
-                except WindowsError:
-                    self.LOG.info("Unable to delete '%s' from the registry:", name)
+            except OSError:
+                with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH, 0, winreg.KEY_ALL_ACCESS) as app_key:
+                    winreg.DeleteValue(app_key, name)
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.info("Unable to delete \"%s\" from the registry:", name)
 
     def get_array_value(self, name):
         try:
             with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH) as app_key:
-                try:
-                    return [str(v) for v in winreg.QueryValueEx(app_key, name)[0]]
-                except:
-                    self.LOG.info("Unable to get array '%s' from the registry:", name, exc_info=True)
+                value, value_type = winreg.QueryValueEx(app_key, name)
+
+                if not value_type == winreg.REG_MULTI_SZ:
+                    raise ValueError("Value must be of REG_MULTI_SZ type!")
+
+                return value
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.info("Unable to get array \"%s\" from the registry:", name, exc_info=True)
 
         return None
 
     def set_array_value(self, name, value):
         try:
             with winreg.OpenKey(self.REGISTRY_KEY, self.CONFIG_PATH, 0, winreg.KEY_WRITE) as app_key:
-                try:
-                    winreg.SetValueEx(app_key, name, 0, winreg.REG_MULTI_SZ, [str(v) for v in value])
-                except:
-                    self.LOG.exception("Unable to set '%s' in the registry:", name)
+                    winreg.SetValueEx(app_key, name, 0, winreg.REG_MULTI_SZ, value)
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.exception("Unable to set \"%s\" in the registry:", name)
 
     def get_dict_value(self, name):
         try:
-            with winreg.OpenKey(self.REGISTRY_KEY, r'{}\{}'.format(self.CONFIG_PATH, key), 0, winreg.KEY_ALL_ACCESS) as app_key:
+            with winreg.OpenKey(self.REGISTRY_KEY, r'{}\{}'.format(self.CONFIG_PATH, name), 0, winreg.KEY_ALL_ACCESS) as app_key:
                 v = {}
 
                 try:
                     i = 0
                     while True:
-                        name, value, _value_type = winreg.EnumValue(app_key, i)
+                        name, value, value_type = winreg.EnumValue(app_key, i)
+
+                        if value_type != winreg.REG_SZ:
+                            raise ("Value must be of REG_SZ type!")
 
                         if value is not None:
-                            v[name] = str(value)
+                            v[name] = value
 
                         i += 1
-                except WindowsError as e:
+                except OSError as e:
                     if e.winerror != ERROR_NO_MORE_ITEMS and e.winerror != ERROR_NO_MORE_FILES:
                         raise
                     else:
                         pass  # end of keys
-                except:
-                    self.LOG.exception("Error during registry enumeration:")
-                    return None
 
-            return v
+                return v
         except:
             self.LOG.info("Unable to get dict '%s' from the registry:")
 
@@ -129,11 +137,9 @@ class RegistryConfig(BaseConfig):
 
     def set_dict_value(self, name, value):
         try:
-            with winreg.CreateKey(self.REGISTRY_KEY, r'{}\{}'.format(self.REGISTRY_PATH, name)) as dict_key:
-                try:
-                    for k, v in value.items():
-                        winreg.SetValueEx(dict_key, str(k), 0, winreg.REG_SZ, str(v))
-                except:
-                    self.LOG.exception("Unable to access registry:")
+            with winreg.CreateKey(self.REGISTRY_KEY, r'{}\{}'.format(self.CONFIG_PATH, name)) as app_key:
+                for k, v in value.items():
+                    winreg.SetValueEx(app_key, k, 0, winreg.REG_SZ, v)
         except:
-            self.LOG.exception("Unable to access registry:")
+            self.LOG.exception("Unable to set \"%s\" in the registry:", name)
+#}
---
+++ test/config/registry_config.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+
+if sys.platform.startswith('win32'):
+    import winreg
+
+    from nativeconfig.config import RegistryConfig
+    from nativeconfig.config.registry_config import traverse_registry_key
+    from nativeconfig.options import StringOption
+
+    from test.config import TestConfigMixin
+
+    class MyRegistryConfig(RegistryConfig):
+        CONFIG_PATH = r'Software\test_config'
+
+    class TestRegistryConfig(unittest.TestCase, TestConfigMixin):
+        CONFIG_TYPE = MyRegistryConfig
+
+        def tearDown(self):
+            try:
+                for k in traverse_registry_key(MyRegistryConfig.REGISTRY_KEY, MyRegistryConfig.CONFIG_PATH):
+                    winreg.DeleteKey(MyRegistryConfig.REGISTRY_KEY, k)
+            except OSError:
+                pass
+
+            TestConfigMixin.tearDown(self)
+
+        def test_config_is_created_if_not_found(self):
+            class MyConfig(MyRegistryConfig):
+                first_name = StringOption('FirstName', default='Ilya')
+
+            with self.assertRaises(OSError):
+                winreg.OpenKey(MyRegistryConfig.REGISTRY_KEY, MyRegistryConfig.CONFIG_PATH)
+
+            MyConfig.get_instance()
+
+            winreg.OpenKey(MyRegistryConfig.REGISTRY_KEY, MyRegistryConfig.CONFIG_PATH)
