b1059a265f7a0b791f3c3582ea65867d0d4d2884
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-03-17 15:34:15 -0700

    Move configuration to setup.cfg
    Enable tests for Python 3.6

--- setup.py
+++ setup.py
@@ -11,28 +11,8 @@ with open(os.path.join(os.path.dirname(__file__), 'nativeconfig', 'version.py'))
 
 
 setup(
-    name='nativeconfig',
     version=VERSION,
     packages=['nativeconfig', 'nativeconfig.options', 'nativeconfig.configs'],
-    url='https://github.com/GreatFruitOmsk/nativeconfig',
-    license='MIT License',
-    author='Ilya Kulakov',
-    author_email='kulakov.ilya@gmail.com',
-    description="Cross-platform python module to store application config via native subsystems such as Windows Registry or NSUserDefaults.",
-    platforms=["Mac OS X 10.6+", "Windows XP+", "Linux 2.6+"],
     keywords='config',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
     test_suite='test'
 )
