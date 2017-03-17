dc32d6aa249cf2db8779c92ae0fea6ece660b489
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-03-17 14:30:44 -0700

    Add EnumOption
    Supports enum.Enum and subclasses.
    Refs #issue-5

---
+++ nativeconfig/options/enum_option.py
@@ -0,0 +1,116 @@
+import enum
+import logging
+
+from .base_option import BaseOption, BaseContainerOption
+from .float_option import FloatOption
+from .int_option import IntOption
+from .string_option import StringOption
+from nativeconfig.exceptions import DeserializationError, ValidationError, InitializationError
+
+
+LOG = logging.getLogger('nativeconfig')
+
+
+class EnumOption(BaseOption):
+    """±
+    EnumOption represents Python Enum in config.
+    """
+    def __init__(self, name, enum_type, value_option=None, **kwargs):
+        """
+        Accepts all the arguments of BaseConfig except choices.
+
+        If value_option is provided, it's used to serialize values. Otherwise Enum names are used.
+
+        @param enum_type: Type that subclasses from enum.Enum that will be used to instantiate python value.
+        @type enum_type: enum.EnumMeta
+
+        @param value_option: An instance of BaseOption subclass that provides serialization and validation.
+            For known Enum types, value_option will be picked automatically unless set explicitly.
+        @type value_option: BaseOption
+        """
+        if issubclass(enum_type, enum.Enum):
+            self._enum_type = enum_type
+        else:
+            raise InitializationError("enum_class must be of a enum.Enum type")
+
+        if value_option:
+            if isinstance(value_option, BaseOption) and not isinstance(value_option, BaseContainerOption):
+                self._value_option = value_option
+            else:
+                raise InitializationError("value_option cannot be a container option")
+        elif issubclass(enum_type, int):
+            self._value_option = IntOption('IntOption')
+        elif issubclass(enum_type, float):
+            self._value_option = FloatOption('FloatOption')
+        elif issubclass(enum_type, str):
+            self._value_option = StringOption('StringOption')
+        else:
+            self._value_option = None
+
+        choices = kwargs.pop('choices', tuple(enum_type.__members__.values()))
+
+        super().__init__(name, choices=choices, **kwargs)
+
+    def serialize(self, python_value):
+        if self._value_option:
+            return self._value_option.serialize(python_value.value)
+        else:
+            return python_value.name
+
+    def deserialize(self, raw_value):
+        """
+        1. If value_option is set, will try to instantiate enum by value
+        2. Otherwise will try to find an appropriate value by comparing string.
+        """
+        if self._value_option:
+            try:
+                return self._enum_type(self._value_option.deserialize(raw_value))
+            except (ValueError, DeserializationError):
+                pass
+
+        LOG.info("Unable to instantiate \"{}\" directly.", self._enum_type)
+
+        raw_value_lower = raw_value.lower()
+
+        for name, value in self._enum_type.__members__.items():
+            if str(value).lower() == raw_value_lower or name.lower() == raw_value_lower:
+                return value
+
+        raise DeserializationError("invalid value for {}".format(self._enum_type), raw_value, self.name)
+
+    def serialize_json(self, python_value):
+        if self._value_option:
+            return self._value_option.serialize_json(python_value.value)
+        else:
+            return super().serialize_json(python_value.name)
+
+    def deserialize_json(self, json_value):
+        if json_value == 'null':
+            return None
+        elif self._value_option:
+            try:
+                enum_value = self._value_option.deserialize_json(json_value)
+            except DeserializationError:
+                enum_value = super().deserialize_json(json_value)
+        else:
+            enum_value = super().deserialize_json(json_value)
+
+        try:
+            return self._enum_type(enum_value)
+        except ValueError:
+            pass
+
+        try:
+            return self._enum_type[enum_value]
+        except KeyError:
+            pass
+
+        raw_value = str(enum_value)
+
+        return self.deserialize(raw_value)
+
+    def validate(self, python_value):
+        super().validate(python_value)
+
+        if not isinstance(python_value, self._enum_type):
+            raise ValidationError("{!r} is not part of {}".format(python_value, self._enum_type), python_value, self.name)
---
+++ test/options/enum_option.py
@@ -0,0 +1,117 @@
+import os
+from enum import Enum
+from pathlib import Path
+import unittest
+
+from nativeconfig import InitializationError
+from nativeconfig import EnumOption, PathOption, IntOption, FloatOption, StringOption, ArrayOption
+
+from test import StubConfig
+from test.options import TestOptionMixin, Option, make_option_type
+
+
+class IntEnum(int, Enum):
+    A = 1
+    B = 2
+
+
+class FloatEnum(float, Enum):
+    A = 1.0
+    B = 2.0
+
+
+class StringEnum(str, Enum):
+    A = 'a'
+    B = 'b'
+
+
+class PathEnum(Enum):
+    A = Path('/a')
+    B = Path('/b')
+
+
+class TestEnumOption(TestOptionMixin, unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.OPTIONS = [
+            Option(
+                option_type=make_option_type(EnumOption, enum_type=IntEnum),
+                value=IntEnum.A,
+                alternate_value=IntEnum.B,
+                invalid_value=FloatEnum.A,
+                invalid_json_value='"string"',
+                invalid_raw_value='string'
+            ),
+            Option(
+                option_type=make_option_type(EnumOption, enum_type=FloatEnum),
+                value=FloatEnum.A,
+                alternate_value=FloatEnum.B,
+                invalid_value='string',
+                invalid_json_value='"string"',
+                invalid_raw_value='string'
+            ),
+            Option(
+                option_type=make_option_type(EnumOption, enum_type=StringEnum),
+                value=StringEnum.A,
+                alternate_value=StringEnum.B,
+                invalid_value=IntEnum.A,
+                invalid_json_value='42',
+                invalid_raw_value=None
+            ),
+            Option(
+                option_type=make_option_type(EnumOption, enum_type=PathEnum, value_option=PathOption('_')),
+                value=PathEnum.A,
+                alternate_value=PathEnum.B,
+                invalid_value=IntEnum.A,
+                invalid_json_value='42',
+                invalid_raw_value=None
+            ),
+            Option(
+                option_type=make_option_type(EnumOption, enum_type=PathEnum),
+                value=PathEnum.A,
+                alternate_value=PathEnum.B,
+                invalid_value=IntEnum.A,
+                invalid_json_value='42',
+                invalid_raw_value=None
+            )
+        ]
+
+    def test_value_option_is_set_for_known_types(self):
+        class MyConfig(StubConfig):
+            int_enum = EnumOption('IntEnum', IntEnum, env_name='TEST_ENUM', default=IntEnum.B)
+            float_enum = EnumOption('FloatEnum', FloatEnum)
+            str_enum = EnumOption('StringEnum', StringEnum)
+            path_enum = EnumOption('PathEnum', PathEnum, value_option=PathOption('_'))
+            path_enum_no_value_option = EnumOption('PathEnum2', PathEnum)
+
+        self.assertIsInstance(MyConfig.int_enum._value_option, IntOption)
+        self.assertIsInstance(MyConfig.float_enum._value_option, FloatOption)
+        self.assertIsInstance(MyConfig.str_enum._value_option, StringOption)
+        self.assertIsNone(MyConfig.path_enum_no_value_option._value_option)
+
+    def test_enum_type_must_subclass_Enum(self):
+        with self.assertRaises(InitializationError):
+            class MyConfig(StubConfig):
+                enum_option = EnumOption('_', int)
+
+    def test_value_option_cannot_be_container(self):
+        with self.assertRaises(InitializationError):
+            class MyConfig(StubConfig):
+                enum_option = EnumOption('_', IntEnum, value_option=ArrayOption('_', value_option=StringOption('_')))
+
+    def test_can_deserialize_from_enum_name(self):
+        class MyConfig(StubConfig):
+            enum_option = EnumOption('_', IntEnum, env_name=self.OPTION_ENV_NAME)
+
+        c = MyConfig.get_instance()
+
+        self.assertIsNone(c.enum_option)
+
+        c.set_value('_', 'A')
+        self.assertEqual(c.enum_option, IntEnum.A)
+
+        del c.enum_option
+        self.assertIsNone(c.enum_option)
+
+        os.environ[self.OPTION_ENV_NAME] = '"A"'
+        self.assertEqual(c.enum_option, IntEnum.A)
