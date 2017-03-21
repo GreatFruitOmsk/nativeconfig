444eeb5c09271ed3dd270867135a1770f3a10130
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-03-20 22:29:31 -0700

    Add support for temporary config in ChainConfig
    It replaces one shot values. Refs #5.

--- nativeconfig/configs/chain_config.py
+++ nativeconfig/configs/chain_config.py
@@ -1,9 +1,14 @@
 from collections import OrderedDict
 
+from .memory_config import MemoryConfig
 from nativeconfig.options import ValueSource
 
 
 class ChainConfig:
+    @classmethod
+    def make_temporary_config(cls, options):
+        return type('TemporaryConfig', (MemoryConfig,), options)()
+
     def __init__(self, configs):
         """
         @type configs: {BaseConfig}
@@ -13,20 +18,44 @@ class ChainConfig:
 
         self._configs_by_name = {}
         self._configs_by_attribute = {}
+        options = {}
 
         for c in configs.values():
-            for name in c._ordered_options_by_name.keys():
-                self._configs_by_name.setdefault(name, []).append(c)
-
-            for attribute_name in c._ordered_options_by_attribute.keys():
+            for attribute_name, option in c._ordered_options_by_attribute.items():
                 self._configs_by_attribute.setdefault(attribute_name, []).append(c)
+                self._configs_by_name.setdefault(option.name, []).append(c)
 
-        for name, config in configs.items():
-            setattr(self, name, config)
+                if attribute_name not in options:
+                    options[attribute_name] = option
+                else:
+                    current_option = options[attribute_name]
+
+                    if current_option.name != option.name:
+                        raise ValueError("name mismatch: expected '{}' but found '{}' in '{}'".format(
+                            current_option.name, option.name, c
+                        ))
+                    elif not isinstance(option, current_option.__class__):
+                        # Foremost configs be specialized options of inferior.
+                        raise ValueError("type mismatch: expected a subclass of '{}' but found '{}' in '{}'".format(
+                            current_option.__class__.__name__, option.__class__.__name__, c
+                        ))
+
+        self._temporary = self.make_temporary_config(options)
+
+    @property
+    def temporary(self):
+        """
+        @rtype: MemoryConfig
+        """
+        return self._temporary
 
     def __getattr__(self, name):
         if name not in self._configs_by_attribute:
             raise AttributeError("'{}' has no attribute '{}'", self.__class__.__name__, name)
+
+        value, source = self._temporary._ordered_options_by_attribute[name].read_value(self._temporary)
+        if source != ValueSource.default and value is not None:
+            return value
 
         default = None
 
--- test/configs/chain_config.py
+++ test/configs/chain_config.py
@@ -3,19 +3,7 @@ import os
 import unittest.mock
 import uuid
 
-from nativeconfig import ChainConfig, MemoryConfig, IntOption
-
-
-class AConfig(MemoryConfig):
-    pass
-
-
-class BConfig(MemoryConfig):
-    pass
-
-
-class CConfig(MemoryConfig):
-    pass
+from nativeconfig import ChainConfig, MemoryConfig, IntOption, StringOption
 
 
 class TestChainConfig(unittest.TestCase):
@@ -51,7 +39,7 @@ class TestChainConfig(unittest.TestCase):
 
         a = AConfig.get_instance()
         b = BConfig.get_instance()
-        c = ChainConfig(OrderedDict(a=a, b=b))
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
 
         b.age = 42
         self.assertEqual(c.age, 42)
@@ -71,7 +59,7 @@ class TestChainConfig(unittest.TestCase):
 
         a = AConfig.get_instance()
         b = BConfig.get_instance()
-        c = ChainConfig(OrderedDict(a=a, b=b))
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
 
         b.age = 9000
         self.assertEqual(c.age, 9000)
@@ -91,7 +79,7 @@ class TestChainConfig(unittest.TestCase):
 
         a = AConfig.get_instance()
         b = BConfig.get_instance()
-        c = ChainConfig(OrderedDict(a=a, b=b))
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
 
         self.assertEqual(c.age, 42)
 
@@ -107,9 +95,86 @@ class TestChainConfig(unittest.TestCase):
 
         a = AConfig.get_instance()
         b = BConfig.get_instance()
-        c = ChainConfig(OrderedDict(a=a, b=b))
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
 
         self.assertEqual(c.age, 42)
 
         os.environ[self.OPTION_ENV_NAME] = BConfig.age.serialize_json(0xdeadbeef)
         self.assertEqual(c.age, 0xdeadbeef)
+
+    def test_temp_value(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            age = IntOption('_')
+
+        a = AConfig.get_instance()
+        b = BConfig.get_instance()
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
+
+        c.temporary.age = 0xdeadbeef
+        self.assertEqual(c.age, 0xdeadbeef)
+
+    def test_temp_value_with_env(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            age = IntOption('_', env_name=self.OPTION_ENV_NAME)
+
+        a = AConfig.get_instance()
+        b = BConfig.get_instance()
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
+
+        c.temporary.age = 0xdeadbeef
+        self.assertEqual(c.age, 0xdeadbeef)
+
+        os.environ[self.OPTION_ENV_NAME] = '42'
+        self.assertEqual(c.age,  0xdeadbeef)
+
+        del c.temporary.age
+        self.assertEqual(c.age, 42)
+
+    def test_temp_takes_options_from_first_appearance(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            age = IntOption('_', env_name=self.OPTION_ENV_NAME)
+
+        a = AConfig.get_instance()
+        b = BConfig.get_instance()
+        c = ChainConfig(OrderedDict([('a', a), ('b', b)]))
+
+        self.assertEqual(c.temporary.__class__.age, AConfig.age)
+
+    def test_option_type_mismatch_raises(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            age = StringOption('_')
+
+        with self.assertRaises(ValueError):
+            ChainConfig(OrderedDict([('a', AConfig.get_instance()), ('b', BConfig.get_instance())]))
+
+    def test_option_name_mismatch_raises(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            age = IntOption('Age')
+
+        with self.assertRaises(ValueError):
+            ChainConfig(OrderedDict([('a', AConfig.get_instance()), ('b', BConfig.get_instance())]))
+
+    def test_option_attibute_name_mismatch_raises(self):
+        class AConfig(MemoryConfig):
+            age = IntOption('_')
+
+        class BConfig(MemoryConfig):
+            height = IntOption('_')
+
+        with self.assertRaises(AttributeError):
+            ChainConfig(OrderedDict([('a', AConfig.get_instance()), ('b', BConfig.get_instance())]))
